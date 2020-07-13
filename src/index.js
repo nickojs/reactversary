9efc9e4c473096efe246c2fc4c9962b9e4bc7fef
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-12 22:44:18 -0300

    Adjusted index to use redux

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
+import userReducer from './store/reducers/user';
+
+const rootReducer = combineReducers({
+  user: userReducer
+});
+
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
