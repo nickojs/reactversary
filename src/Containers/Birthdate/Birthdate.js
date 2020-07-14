60e2f67d1ef8808288551d665308e14f72e478a7
nicko <62088609+nickojs@users.noreply.github.com>
2020-07-14 13:19:54 -0300

    Improved dashboard menu

--- src/Containers/Birthdate/Birthdate.js
+++ src/Containers/Birthdate/Birthdate.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+
+import IconButton from '@material-ui/core/IconButton';
 import CakeIcon from '@material-ui/icons/Cake';
 import ListIcon from '@material-ui/icons/List';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import { logout } from '../../store/actions/user';
 
 import { useStyles } from './styles';
 
@@ -18,14 +25,49 @@ import BirthdatesList from '../../components/birthdatesList/birthdatesList';
 const Dashboard = () => {
   const classes = useStyles();
   const { token } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (e) => setAnchorEl(e.currentTarget);
+  const handleClose = () => setAnchorEl(null);
+  const logoutHandler = () => dispatch(logout());
 
   return (
     <div className={classes.root}>
       <CssBaseline />
       <AppBar position="absolute" className={classes.appBar}>
-        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-          Dashboard
-        </Typography>
+        <Toolbar>
+          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+            Dashboard
+          </Typography>
+
+          <div>
+            <IconButton onClick={handleMenu}>
+              <AccountCircle />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorEl}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right'
+              }}
+              open={Boolean(anchorEl)}
+              onClose={handleClose}
+            >
+              <MenuItem onClick={handleClose}>Profile</MenuItem>
+              <MenuItem onClick={logoutHandler}>Logout</MenuItem>
+            </Menu>
+
+          </div>
+
+        </Toolbar>
       </AppBar>
       <main className={classes.content}>
         <div className={classes.appBarSpacer} />
